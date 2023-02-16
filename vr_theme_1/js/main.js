ba99324f3d77046c81b5b9993167924565c32f66
madhav1521 <72124402+madhav1521@users.noreply.github.com>
2023-02-16 10:30:03 +0530

    Delete vr_theme_1 directory

--- vr_theme_1/js/main.js
+++
@@ -1,6 +0,0 @@
-// JS Which needs to add
-document.write(	
-	'<script src="js/jquery.js"></script>'+	
-	'<script src="js/bootstrap.bundle.min.js"></script>'+
-	'<script src="js/custom.js"></script>'
-	);
